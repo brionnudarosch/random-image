ff88fac228c9f63951bf23b18c1160d7f64886d6
brionnudarosch <brion@awesomefun.xyz>
2025-05-13 16:50:05 -0700

    Add files via upload

---
+++ obfuscated-image-urls.js
@@ -0,0 +1,4 @@
+const base64Urls = [
+  "aHR0cDovL2V4YW1wbGUuY29tL2ltYWdlMS5qcGc=",
+  "aHR0cDovL2V4YW1wbGUuY29tL2ltYWdlMi5qcGc="
+];
---
+++ source-map.js
@@ -0,0 +1,4 @@
+const sourceMap = {
+  "aHR0cDovL2V4YW1wbGUuY29tL2ltYWdlMS5qcGc=": "https://www.are.na/block/123456",
+  "aHR0cDovL2V4YW1wbGUuY29tL2ltYWdlMi5qcGc=": "https://www.are.na/block/654321"
+};
