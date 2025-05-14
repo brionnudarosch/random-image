0dc5615c66d7d9f20c376f6b5b3de6d2fa5069c0
brionnudarosch <brion@awesomefun.xyz>
2025-05-13 18:20:01 -0700

    Update source-map.js

--- source-map.js
+++ source-map.js
@@ -1,5 +1,4 @@
 const sourceMap = {
-  "aHR0cHM6Ly9kMnc5cm5mY3k3bW03OC5jbG91ZGZyb250Lm5ldC8xMjM0NTYvb3JpZ2luYWxfaW1nMS5qcGc=": "https://www.are.na/block/123456",
-  "aHR0cHM6Ly9kMnc5cm5mY3k3bW03OC5jbG91ZGZyb250Lm5ldC83ODkxMDExL29yaWdpbmFsX2ltZzIucG5n": "https://www.are.na/block/7891011",
-  "aHR0cHM6Ly9kMnc5cm5mY3k3bW03OC5jbG91ZGZyb250Lm5ldC8xMjEzMTQvb3JpZ2luYWxfaW1nMy5naWY=": "https://www.are.na/block/121314"
+  "aHR0cHM6...": "https://www.are.na/block/XXXXX",
+  ...
 };
